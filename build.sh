2a788e2e22403bbdde9452a0b8358fae62bbf5f1
xixixao <xixixao@seznam.cz>
2024-03-30 22:15:38 +0100

    Fix the replace command

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ mv dist/* ../recomputer.github.io/ &&
 rm -r dist &&
 cd ../recomputer.github.io &&
 touch .nojekyll &&
-perl -pi -w -e 's&<!--stats-->&<script defer src='\"'\"'https://static.cloudflareinsights.com/beacon.min.js'\"'\"' data-cf-beacon='\"'\"'{\"token\": \"0701565b4e494a109d62174c015e387d\"}'\"'\"'></script>&' index.html &&
+perl -pi -w -e "s{<!--stats-->}{<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{\"token\": \"0701565b4e494a109d62174c015e387d\"}'></script>}" index.html &&
 git add . &&
 git commit -m 'Built'
